060c7d32cfca755e6f5e40b26fd8557e9bc2bccb
Tim0412 <tim0412@gmail.com>
2017-05-25 21:40:08 +0800

    “你做了哪些改變”

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(function () {
+
+    //new WOW().init();
+    $('nav a, #scrollTopBtn,header a').bind('click', function (event) {
+        var $anchor = $(this);
+        //宣告navbarHeight = 50
+        var navbarHeight = 50;
+        //宣告動畫的秒數為1250
+        var animationDuration = 1250;
+        //宣告timingFunction = 'easeInOutCubic'
+        var timingFunction = 'easeInOutCubic';
+
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - navbarHeight)
+        }, animationDuration, timingFunction);
+        event.preventDefault();
+    });
+
+    var navHeight = $('nav').height();
+
+    // window在捲動的時候
+    $(window).scroll(function () {
+        // 以上條件發生時所做的處理
+        // 把$(window).scrollTop()存到$scrollPosition
+        var $scrollPosition = $(window).scrollTop();
+        // 把()的內容印到console裡面
+        console.log($scrollPosition);
+
+        if ($scrollPosition > navHeight) {
+            $('nav').addClass('filled');
+        } else {
+            $('nav').removeClass('filled');
+        }
+
+        //如果$scrollPosition大於500
+        if ($scrollPosition > 100) {
+            //#scrollTopBtn加上class scroll
+            //$('#scrollTopBtn').addClass('scroll');
+            $('#scrollTopBtn').fadeIn();
+        } else {
+            //#scrollTopBtn去除class scroll
+            //沒有了scroll的#scrollTopBtn display為none
+            //$('#scrollTopBtn').removeClass('scroll');
+            $('#scrollTopBtn').fadeOut();
+        }
+
+        if ($scrollPosition > 20) {
+            $('#header').addClass('scroll');
+        } else {
+            $('#header').removeClass('scroll');
+        }
+
+    });
+
+
+});
